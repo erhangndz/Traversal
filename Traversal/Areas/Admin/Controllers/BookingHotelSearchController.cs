8fbcfe4249d6f9073d70b9b1e4e1f641247d55ca
Erhan Gündüz <erhangndz@live.com>
2023-08-21 23:09:12 +0300

    lesson 72

---
+++ Traversal/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Traversal.Areas.Admin.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BookingHotelSearchController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/search?checkin_date=2023-09-27&dest_type=city&units=metric&checkout_date=2023-09-28&adults_number=2&order_by=popularity&dest_id=-1456928&filter_by_currency=EUR&locale=tr&room_number=1&children_number=2&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&page_number=0&include_adjacency=true"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "e782ab3024msh2e78af442950fd0p115136jsnc3dd0b8c5f54" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+               var bodyReplace= body.Replace(".", "");
+                var values = JsonConvert.DeserializeObject<BookingSearchHotelViewModel>(bodyReplace);
+                return View(values.result);
+            }
+        }
+
+
+        [HttpGet]
+        public  IActionResult GetCityID()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetCityID(string p)
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={p}&locale=tr"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "e782ab3024msh2e78af442950fd0p115136jsnc3dd0b8c5f54" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<SearchLocationViewModel>>(body);
+
+                return View();
+            }
+        }
+    }
+}
---
+++ Traversal/Areas/Admin/Models/SearchLocationViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Traversal.Areas.Admin.Models
+{
+    public class SearchLocationViewModel
+    {
+
+        
+       
+        
+            public int nr_hotels { get; set; }
+            public string city_name { get; set; }
+            public float latitude { get; set; }
+            public int? city_ufi { get; set; }
+            public string region { get; set; }
+            public string lc { get; set; }
+            public string country { get; set; }
+            public string name { get; set; }
+            public string timezone { get; set; }
+            public string image_url { get; set; }
+            public string type { get; set; }
+            public string cc1 { get; set; }
+            public int hotels { get; set; }
+            public float longitude { get; set; }
+            public string dest_type { get; set; }
+            public string dest_id { get; set; }
+            public int rtl { get; set; }
+            
+            public string label { get; set; }
+        
+
+     
+
+    }
+}
